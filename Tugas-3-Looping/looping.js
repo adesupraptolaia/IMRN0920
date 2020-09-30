20c0662152727ce4bdb7ee35bdde1232ffd2b833
adesupraptolaia <ades@alterra.id>
2020-10-01 06:08:12 +0700

    prettier looping.js

--- Tugas-3-Looping/looping.js
+++ Tugas-3-Looping/looping.js
@@ -1,79 +1,69 @@
-console.log("No. 1 Looping While")
+console.log("No. 1 Looping While");
 
-let i = 1
+let i = 1;
 
 while (i <= 20) {
-    
     if (i <= 10) {
         if (i == 1) {
-            console.log("LOOPING PERTAMA")
+            console.log("LOOPING PERTAMA");
         }
 
-        console.log(i*2 + " - I love coding")
-
+        console.log(i * 2 + " - I love coding");
     } else {
         if (i == 11) {
-            console.log("LOOPING KEDUA")
+            console.log("LOOPING KEDUA");
         }
-        
 
-        console.log(42-i*2 + " - I will become a mobile developer")
+        console.log(42 - i * 2 + " - I will become a mobile developer");
     }
 
-    i++
+    i++;
 }
 
-console.log("\n\nNo. 2 Looping menggunakan for")
+console.log("\n\nNo. 2 Looping menggunakan for");
 
 for (let i = 1; i <= 20; i++) {
-
     if (i % 2 === 0) {
-        console.log(i + " - Berkualitas")
-
+        console.log(i + " - Berkualitas");
     } else if (i % 3 === 0) {
-        console.log(i + " - I Love Coding")
-
+        console.log(i + " - I Love Coding");
     } else {
-        console.log(i + " - Santai")
-    } 
+        console.log(i + " - Santai");
+    }
 }
 
-
-console.log("\n\nNo. 3 Membuat Persegi Panjang")
+console.log("\n\nNo. 3 Membuat Persegi Panjang");
 
 for (let row = 0; row < 4; row++) {
-    let pagar = '';
+    let pagar = "";
     for (let col = 0; col < 8; col++) {
-        pagar += "#"
+        pagar += "#";
     }
-    console.log(pagar)
+    console.log(pagar);
 }
 
-
-console.log("\n\nNo. 4 Membuat Tangga")
+console.log("\n\nNo. 4 Membuat Tangga");
 
 for (let row = 1; row <= 7; row++) {
-    let tangga = ''
+    let tangga = "";
     for (let col = 1; col <= row; col++) {
-        tangga += "#"
+        tangga += "#";
     }
-    console.log(tangga)
+    console.log(tangga);
 }
 
-
-console.log("\n\nNo. 5 Membuat Papan Catur")
+console.log("\n\nNo. 5 Membuat Papan Catur");
 
 for (let row = 1; row <= 8; row++) {
-    let papanCatur = ""
+    let papanCatur = "";
 
     for (let col = 1; col <= 8; col++) {
-        if ( (row+col)%2 == 0) {
-            papanCatur += " "
-
+        if ((row + col) % 2 == 0) {
+            papanCatur += " ";
         } else {
-            papanCatur += "#"
+            papanCatur += "#";
         }
     }
-    
-    console.log(papanCatur)
+
+    console.log(papanCatur);
 }
